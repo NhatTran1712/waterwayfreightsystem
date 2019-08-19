59891c442217ad4afce319d04173c4f38a28887b
NhatTran <nhat.tranthinguyen@gmail.com>
2019-08-19 12:18:32 +0700

    Re-setup project

--- src/main/java/org/apptopia/waterwayfreightsystem/api/api/core/model/TravelProblem.java
+++ src/main/java/org/apptopia/waterwayfreightsystem/api/api/core/model/TravelProblem.java
@@ -1,5 +1,10 @@
 package org.apptopia.waterwayfreightsystem.api.api.core.model;
 
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Entity @Data
 public class TravelProblem {
 
 }
--- src/main/java/org/apptopia/waterwayfreightsystem/api/api/infrastructure/PostgresCargoRepositoryImpl.java
+++ src/main/java/org/apptopia/waterwayfreightsystem/api/api/infrastructure/PostgresCargoRepositoryImpl.java
@@ -2,8 +2,6 @@ package org.apptopia.waterwayfreightsystem.api.api.infrastructure;
 
 import org.apptopia.waterwayfreightsystem.api.api.core.model.Cargo;
 import org.apptopia.waterwayfreightsystem.api.api.core.model.CargoRepository;
-import org.apptopia.waterwayfreightsystem.api.api.ship.model.Ship;
-import org.apptopia.waterwayfreightsystem.api.api.ship.model.ShipRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
